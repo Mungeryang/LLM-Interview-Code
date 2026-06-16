471e615d1c8302f844323fb639a865816de34f17
mungermiao <1875706036@qq.com>
2026-06-16 21:29:08 +0800

    upload ListNode.py

---
+++ fucking-leetcode/basic/Listnode.py
@@ -0,0 +1,12 @@
+# 单链表实现
+class ListNode:
+	def __init__(self,x):
+		self.val = x
+		self.next = None
+
+
+class Node:
+	def __init__(self, prev, element, next):
+		self.val = element
+		self.next = next
+		self.prev = prev
