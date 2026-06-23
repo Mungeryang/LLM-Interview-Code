57b1693bbf9db1ee0e91d9d3855ff724f71a2008
mungermiao <1875706036@qq.com>
2026-06-23 22:35:47 +0800

    upload tree.py

--- fucking-leetcode/basic/tree.py
+++ fucking-leetcode/basic/tree.py
@@ -10,3 +10,22 @@ def traverse(root: TreeNode):
 		return 
 	traverse(root.left)
 	traverse(root.right)
+
+
+# 层序遍历
+
+from collectios import deque
+
+def levelOrderTraverse(root):
+	if root is None:
+		return
+	q = deque()
+	q.append(root)
+	while q:
+		cur = q.popleft()
+
+		if cur.left is not None:
+			q.append(cur.left)
+		if cur.right is not None:
+			q.append(cur.right)
+
